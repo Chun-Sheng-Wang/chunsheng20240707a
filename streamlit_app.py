7adc632c813f2f88374980834ae4e5fcf89604f2
Chun-Sheng-Wang <125810287+Chun-Sheng-Wang@users.noreply.github.com>
2024-07-07 01:46:10 +0000

    0945

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).中文測試1234"
-)
+if "counter" not in st.session_state:
+    st.session_state.counter = 0
+
+st.session_state.counter += 1
+
+st.header(f"This page has run {st.session_state.counter} times.")
+st.button("Run it again")
