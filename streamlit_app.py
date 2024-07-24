7ddce255c926b2d3e4b147053ef7e86cbcacce3a
Chun-Sheng-Wang <125810287+Chun-Sheng-Wang@users.noreply.github.com>
2024-07-24 07:40:34 +0000

    1234

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import numpy as np
 if "df" not in st.session_state:
     st.session_state.df = pd.DataFrame(np.random.randn(20, 2), columns=["x", "y"])
 
-st.header("Choose a datapoint color")
+st.header("Choose a datapoint color 1234")
 color = st.color_picker("Color", "#FF0000")
 st.divider()
 st.scatter_chart(st.session_state.df, x="x", y="y", color=color)
